c6a5136088604e3ac632c66744ed77a29516b584
Naveen <alapatinaveen18@gmail.com>
2025-03-22 13:41:58 +0530

    Add files via upload
    Contact model added

---
+++ models/Contact.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    subject: String,
+    message: String
+});
+
+const ContactModel = mongoose.model('contact', ContactSchema);
+
+export default ContactModel;
